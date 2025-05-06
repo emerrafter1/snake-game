3de7c193f18305fe993e78540e97def93ffce773
emerrafter1 <emerlouiserafter@gmail.com>
2025-05-06 16:28:53 +0100

    add extend snake method

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ while game_over == False:
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
+        snake.extend()
 
 
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
--- snake.py
+++ snake.py
@@ -17,12 +17,8 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_square = Turtle(shape="square")
-            new_square.color("white")
-            new_square.penup()
-            new_square.goto(position)
-            new_square.speed("slowest")
-            self.segments.append(new_square)
+            self.add_segment(position)
+
 
     def move(self):
         for seg in range(len(self.segments)-1,  0, -1):
@@ -30,6 +26,18 @@ class Snake:
             new_y = self.segments[seg-1].ycor()
             self.segments[seg].goto(new_x, new_y)
         self.head.fd(MOVE_DISTANCE)
+
+    def add_segment(self, position):
+        new_square = Turtle(shape="square")
+        new_square.color("white")
+        new_square.penup()
+        new_square.goto(position)
+        new_square.speed("slowest")
+        self.segments.append(new_square)
+
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def up(self):
         if self.head.heading() != DOWN:
