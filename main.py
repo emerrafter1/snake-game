f6bcffe8a59b31a8327235bca6c6b4ad56857b22
emerrafter1 <emerlouiserafter@gmail.com>
2025-05-06 15:23:42 +0100

    add score class

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 from snake import Snake
 import time
 from food import Food
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -14,6 +15,7 @@ game_over = False
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -35,6 +37,7 @@ while game_over == False:
     #Detect snake has ate food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
 
 
 
