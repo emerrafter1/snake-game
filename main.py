da8ea9aaf9f8a023ce9f4a640930eb0a5bcbac06
emerrafter1 <emerlouiserafter@gmail.com>
2025-05-06 10:28:49 +0100

    create snake

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 
 
+starting_positions = [(0,0), (-20, 0), (-40, 0)]
+
+for position in starting_positions:
+    new_square = Turtle(shape="square")
+    new_square.color("white")
+    new_square.goto(position)
+
+
 
 
 screen.exitonclick()
